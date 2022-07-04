ee5d6dbbd34c8a73149f626a1c678a5523355287
418teapot <wangshuo2912@foxmail.com>
2022-07-04 17:17:14 +0800

    fix(core): fix mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -39,7 +39,7 @@ M.general = {
 
     ["<leader>tt"] = {
       function()
-        require("onedark").toggle()
+        require("core.utils").toggle_theme()
       end,
       " toggle theme",
     }
@@ -53,15 +53,15 @@ M.bufferline = {
   n = {
     ["<Tab>"] = { "<cmd> BufferLineCycleNext <CR>" , " goto next buffer" },
     ["<S-Tab>"] = { "<cmd> BufferLineCyclePrev <CR>", " goto prev buffer" },
-    ["<leader>1"] = {"<cmd> BufferLineGoToBuffer 1 <CR>", "open buffer 1"},
-    ["<leader>2"] = {"<cmd> BufferLineGoToBuffer 2 <CR>", "open buffer 2"},
-    ["<leader>3"] = {"<cmd> BufferLineGoToBuffer 3 <CR>", "open buffer 3"},
-    ["<leader>4"] = {"<cmd> BufferLineGoToBuffer 4 <CR>", "open buffer 4"},
-    ["<leader>5"] = {"<cmd> BufferLineGoToBuffer 5 <CR>", "open buffer 5"},
-    ["<leader>6"] = {"<cmd> BufferLineGoToBuffer 6 <CR>", "open buffer 6"},
-    ["<leader>7"] = {"<cmd> BufferLineGoToBuffer 7 <CR>", "open buffer 7"},
-    ["<leader>8"] = {"<cmd> BufferLineGoToBuffer 8 <CR>", "open buffer 8"},
-    ["<leader>9"] = {"<cmd> BufferLineGoToBuffer 9 <CR>", "open buffer 9"},
+    ["<leader>1"] = { "<cmd> BufferLineGoToBuffer 1 <CR>", "open buffer 1" },
+    ["<leader>2"] = { "<cmd> BufferLineGoToBuffer 2 <CR>", "open buffer 2" },
+    ["<leader>3"] = { "<cmd> BufferLineGoToBuffer 3 <CR>", "open buffer 3" },
+    ["<leader>4"] = { "<cmd> BufferLineGoToBuffer 4 <CR>", "open buffer 4" },
+    ["<leader>5"] = { "<cmd> BufferLineGoToBuffer 5 <CR>", "open buffer 5" },
+    ["<leader>6"] = { "<cmd> BufferLineGoToBuffer 6 <CR>", "open buffer 6" },
+    ["<leader>7"] = { "<cmd> BufferLineGoToBuffer 7 <CR>", "open buffer 7" },
+    ["<leader>8"] = { "<cmd> BufferLineGoToBuffer 8 <CR>", "open buffer 8" },
+    ["<leader>9"] = { "<cmd> BufferLineGoToBuffer 9 <CR>", "open buffer 9" },
   }
 }
 
@@ -70,6 +70,12 @@ M.nvimtree = {
     ["<C-n>"] = { "<cmd> NvimTreeToggle <CR>", "פּ toggle nvimtree" },
     ["<leader>e"] = { "<cmd> NvimTreeFocus <CR>", "פּ focus nvimtree"}
   },
+}
+
+M.whichkey = {
+  n = {
+    ["<leader>wk"] = { "<cmd> WhichKey <CR>", "﬜ which-key all keymaps" }
+  }
 }
 
 return M
