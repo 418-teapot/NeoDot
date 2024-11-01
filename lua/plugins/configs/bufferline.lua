3ab675a945123b411c7ba69cd56eb79e4660136d
418teapot <wangshuo2912@foxmail.com>
2024-11-01 18:06:01 +0800

    plugins: add bufferline visible numbers

--- lua/plugins/configs/bufferline.lua
+++ lua/plugins/configs/bufferline.lua
@@ -8,6 +8,15 @@ local options = {
         text_align = "left",
       },
     }, -- offsets
+    numbers = function(opts)
+      local state = require("bufferline.state")
+      for i, buf in ipairs(state.visible_components) do
+        if buf.id == opts.id then
+          return i
+        end
+      end
+      return opts.ordinal
+    end, -- numbers
   },
 }
 
