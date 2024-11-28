759f6bc514a2e914aa38b0a76d15696ac4dbc926
418teapot <wangshuo2912@foxmail.com>
2024-11-28 17:08:41 +0800

    plugins: fix some vim.cmd

--- lua/plugins/menu.lua
+++ lua/plugins/menu.lua
@@ -4,7 +4,7 @@ local keys = {
   {
     "<RightMouse>",
     function()
-      vim.cmd.exec '"normal! \\<RightMouse>"'
+      vim.cmd.exec('"normal! \\<RightMouse>"')
       local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
       require("menu").open(options, { mouse = true })
     end,
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -8,7 +8,7 @@ local plugin = {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here.
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
