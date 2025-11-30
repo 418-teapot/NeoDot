5483f138c5aa6d640db7f173f1a4a841aa9de5db
418teapot <wangshuo2912@foxmail.com>
2025-11-30 17:06:26 +0800

    fix(plugins): fix telescope settings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -75,8 +75,6 @@ local options = {
     file_ignore_patterns = {
       ".git/",
     },
-    file_sorter = require("telescope.sorters").get_fuzzy_file,
-    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
     path_display = {
       "truncate",
     },
