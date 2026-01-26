264118f2137a339e415ca640ce9863391d4cb734
418teapot <wangshuo2912@foxmail.com>
2026-01-26 19:35:27 +0800

    fix(plugins): fix telescope layout

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -81,7 +81,18 @@ local options = {
     set_ebv = {
       ["COLORTERM"] = "truecolor",
     },
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+      },
+      width = 0.87,
+      height = 0.80,
+    },
   },
+
+  extensions_list = { "themes", "terms" },
+  extensions = {},
 }
 
 local plugin = {
