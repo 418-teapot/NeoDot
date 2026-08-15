605627eb5ad7a13c94328dde84f70b1bcf816192
418teapot <wangshuo2912@foxmail.com>
2026-08-15 18:22:03 +0800

    feat(plugins): migrate treesitter to nvim-treesitter main branch
    CONTEXT: - nvim-treesitter master froze and dropped Neovim 0.12
               support; main branch is a rewrite without configs.setup.
    
    CHANGE:  - Pins nvim-treesitter and textobjects to branch main.
             - Installs missing parsers asynchronously via TS.install.
             - Highlights via built-in vim.treesitter.start on FileType.
             - Uses select_textobject API for af/if/ac/ic keymaps.
             - Drops incremental_selection and verilog parser.
    
    WHY:     - Neovim 0.12 provides highlight/fold/selection natively;
               the plugin only keeps parser installation.
    
    IMPACT:  - Requires tree-sitter-cli >= 0.26.1 and a C compiler.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,119 +1,118 @@
 -- All plugins have lazy = true by default.
 
-local options = {
-  -- if you want to install the parsers to a custom directory
-  -- you can use `parser_install_dir`
-  -- the default path is:
-  -- ~/.local/share/nvim/lazy/nvim-treesitter/parser
-  ensure_installed = {
-    "bash",
-    "bibtex",
-    "c",
-    "c_sharp",
-    "cmake",
-    "comment",
-    "cpp",
-    "css",
-    "cuda",
-    "dockerfile",
-    "go",
-    "haskell",
-    "html",
-    "java",
-    "javascript",
-    "json",
-    "latex",
-    "llvm",
-    "lua",
-    "make",
-    "markdown",
-    "mlir",
-    "nu",
-    "ocaml",
-    "perl",
-    "python",
-    "ruby",
-    "rust",
-    "scala",
-    "tablegen",
-    "typescript",
-    "toml",
-    "verilog",
-    "vim",
-    "vimdoc",
-    "yaml",
-  }, -- ensure_installed
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-  }, -- highlight
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<CR>",
-      node_incremental = "<CR>",
-      scope_incremental = "<TAB>",
-      node_decremental = "<BS>",
-    }, -- keymap
-  }, -- incremental_selection
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-    }, -- select
-  }, -- textobjects
-} -- options
+-- Parsers to install with the `nvim-treesitter` **main** branch.
+-- Highlighting and folding are handled by Neovim 0.12 built-ins,
+-- so this list only controls parser installation.
+local ensure_installed = {
+  "bash",
+  "bibtex",
+  "c",
+  "c_sharp",
+  "cmake",
+  "comment",
+  "cpp",
+  "css",
+  "cuda",
+  "dockerfile",
+  "go",
+  "haskell",
+  "html",
+  "java",
+  "javascript",
+  "json",
+  "latex",
+  "llvm",
+  "lua",
+  "make",
+  "markdown",
+  "mlir",
+  "nu",
+  "ocaml",
+  "perl",
+  "python",
+  "ruby",
+  "rust",
+  "scala",
+  "tablegen",
+  "typescript",
+  "typst",
+  "toml",
+  "vim",
+  "vimdoc",
+  "yaml",
+} -- ensure_installed
 
-local init = function()
-  local present, treesitter = pcall(require, "nvim-treesitter.configs")
+local config = function()
+  local TS = require("nvim-treesitter")
 
-  if not present then
-    return
+  -- cache of installed parsers: name -> true
+  local installed = {}
+  local query_cache = {} -- key: "lang:query" -> boolean
+
+  local function refresh_installed()
+    installed = {}
+    for _, lang in ipairs(TS.get_installed("parsers")) do
+      installed[lang] = true
+    end
+  end
+  refresh_installed()
+
+  -- Install missing parsers asynchronously (does not block startup).
+  local missing = vim.tbl_filter(function(lang)
+    return not installed[lang]
+  end, ensure_installed)
+  if #missing > 0 then
+    TS.install(missing, { summary = true }):await(function()
+      refresh_installed()
+    end)
   end
 
-  treesitter.setup(options)
-
-  local parsers
-  present, parsers = pcall(require, "nvim-treesitter.parsers")
-
-  if not present then
-    return
+  -- Simplified version of LazyVim's `have()`: check that the parser for the
+  -- filetype is installed and (optionally) that the query group exists.
+  local function have(ft, query)
+    local lang = vim.treesitter.language.get_lang(ft)
+    if lang == nil or not installed[lang] then
+      return false
+    end
+    if query then
+      local key = lang .. ":" .. query
+      if query_cache[key] == nil then
+        query_cache[key] = vim.treesitter.query.get(lang, query) ~= nil
+      end
+      return query_cache[key]
+    end
+    return true
   end
 
-  parsers.get_parser_configs().typst = {
-    install_info = {
-      url = "https://github.com/SeniorMars/tree-sitter-typst",
-      files = { "src/parser.c", "src/scanner.c" },
-      branch = "main",
-    },
-    filetype = "typ",
-  }
+  -- Highlighting via Neovim 0.12 built-in `vim.treesitter.start`.
+  vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup(
+      "treesitter_highlight",
+      { clear = true }
+    ),
+    callback = function(ev)
+      if have(ev.match, "highlights") then
+        vim.treesitter.start(ev.buf)
+      end
+    end,
+  })
 end
 
 local plugins = {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "BufEnter",
+    branch = "main",
+    event = "VeryLazy",
     cmd = {
       "TSInstall",
-      "TSBufEnable",
-      "TSBufDisable",
-      "TSModuleInfo",
+      "TSInstallFromGrammar",
+      "TSUninstall",
+      "TSUpdate",
+      "TSLog",
     },
     build = ":TSUpdate",
-    init = init,
-    configs = function()
-      vim.wo.foldmethod = "expr"
-      vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
-    end,
+    config = config,
     dependencies = {
-      "lukas-reineke/indent-blankline.nvim",
       {
         "nvim-treesitter/nvim-treesitter-context",
         opts = function()
@@ -125,6 +124,28 @@ local plugins = {
       },
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        config = function()
+          require("nvim-treesitter-textobjects").setup({
+            select = {
+              lookahead = true,
+            },
+          })
+
+          local select = require("nvim-treesitter-textobjects.select")
+          vim.keymap.set({ "x", "o" }, "af", function()
+            select.select_textobject("@function.outer", "textobjects")
+          end)
+          vim.keymap.set({ "x", "o" }, "if", function()
+            select.select_textobject("@function.inner", "textobjects")
+          end)
+          vim.keymap.set({ "x", "o" }, "ac", function()
+            select.select_textobject("@class.outer", "textobjects")
+          end)
+          vim.keymap.set({ "x", "o" }, "ic", function()
+            select.select_textobject("@class.inner", "textobjects")
+          end)
+        end,
       },
     },
   },
